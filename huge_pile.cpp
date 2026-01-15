c5fa1f60cfcd1dfa48d3fa1c33bc776e34093369
mmacha <macha.v.s.sai.pavan.kumar.mac@oracle.com>
2026-01-15 11:57:34 +0530

    another-contest

---
+++ huge_pile.cpp
@@ -0,0 +1,127 @@
+
+/*
+ " I AM VENGEANCE , I AM THE KNIGHT , I AM THE BATMAN ! "
+____ __ __ __ __ __ ___                             ___ __ __ __ __ __ ____
+ `-._:  .:'   `:::  .:\           |\__/|           /::  .:'   `:::  .:.-'
+     \      :          \          |:   |          /         :       /
+      \     ::    .     `-_______/ ::   \_______-'   .      ::   . /
+       |  :   :: ::'  :   :: ::'  :   :: ::'      :: ::'  :   :: :|
+       |     ;::         ;::         ;::         ;::         ;::  |
+       |  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:|
+       /     :           :           :           :           :    \
+      /______::_____     ::    .     ::    .     ::   _____._::____\
+                    `----._:: ::'  :   :: ::'  _.----'
+                           `--.       ;::  .--'
+                               `-. .:'  .-'
+                                  \    /
+                                   \  /
+                                    \/
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f first
+#define s second
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+
+using ll = long long;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+
+const int MOD=1000000007;
+const int dx[4] = {1, 0, -1, 0},
+          dy[4] = {0, 1, 0, -1};
+
+
+/*--------------------No.of digits------------------*/
+int cntDig(int n) {
+	if(n==0)
+	return 1;
+
+	return floor(log10(n)+1);
+}
+
+
+
+/*------------------isPrime--------------*/
+int isPrime(int n) {
+    bool h=0;
+    for (int i=2;i*i<=n;i++) {
+        if (n%i==0) {
+            h=1;
+        }
+    }
+    return h==0;
+}
+
+
+
+/*-----------------------check palindrome---------------------------*/
+bool check(string s) {
+	int n=s.size();
+	for(int i=0;i<n/2;i++){
+		if(s[i]!=s[n-i-1])
+		return 0;
+	}
+	return 1;
+}
+
+
+
+/*------------------------print "Yes/No"-------------------*/
+void print(bool h){
+	if(h)
+	cout<<"Yes"<<endl;
+	else
+	cout<<"No"<<endl;
+}
+
+
+
+/*------------------------Solve-----------------------------*/
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int T;
+  cin>>T;
+  while(T--){
+    int n,k;
+    cin>>n>>k;
+    
+    queue<pair<int,int>>q;
+    q.push({n,0});
+    int cnt=0,ans=-1;
+    while(!q.empty()){
+    	int s=q.front().first;
+    	int cnt=q.front().second;
+    	q.pop();
+
+    	int l=s/2;
+    	int r=l;
+    	if(s&1 and s!=1) r=l+1;
+    	if(l==k or r==k){
+    		ans=cnt+1;
+    		break;
+    	}
+
+    	if(l==1) continue;
+
+    	q.push({l,cnt+1});
+    	if(r) q.push({r,cnt+1});
+    }
+
+    cout<<ans<<endl;
+
+  }
+  return 0;
+}
+
+/*
+Problem:
+*/
